ec99a73227503b017970039981ece465a54d5a54
jamesxu <xxj8711@gmail.com>
2021-07-03 12:44:53 +0800

    function: wechat scan qrcode

--- web/src/screen/Login.js
+++ web/src/screen/Login.js
@@ -3,15 +3,29 @@ import {inject} from 'mobx-react';
 import {withRouter, Link} from 'react-router-dom';
 import {Button, Intent, Spinner } from '@blueprintjs/core';
 import SoloLayout from '../component/SoloLayout';
+import QRCode from 'qrcode.react';
 
 @withRouter
 @inject("store")
 
 export default class Login extends Component
 {
+    state={"qrcode": false}
+    async componentDidMount()
+    {
+        const ret = await this.props.store.get_qrcode();
+        // console.log(ret)
+        if (ret && ret.url) {
+            this.setState({'qrcode': ret.url});
+        }
+    }
     render()
     {
-        return <SoloLayout title="登入">Login Form</SoloLayout>
+        return <SoloLayout title="登入">
+            <div className="code-box">
+                {this.state.qrcode ? <><QRCode value={this.state.qrcode} /> <div className="gray subtitle">微信扫码登录</div></> :<Spinner/>}
+            </div>
+        </SoloLayout>
         // return <h1>Login form</h1>;
     }
 } 
--- web/src/store/AppState.js
+++ web/src/store/AppState.js
@@ -1,7 +1,9 @@
 import { observable } from "mobx";
+import axios from 'axios';
 
 class AppState
 {
+    api_base = 'http://127.0.0.1:8080';
     @observable appname = "EasyStarter";
     @observable top_menu_items = [
         {"text": "首页", "link": "/"},
@@ -9,6 +11,11 @@ class AppState
         {"text": "第三方应用和服务", "link": "/apps"},
         {"text": "意见反馈", "link": "/feedback"},
     ];
+    async get_qrcode()
+    {
+        const {data} = await axios.get(this.api_base + '/qrcode');
+        return data || false;
+    }
 }
 
 export default new AppState();
