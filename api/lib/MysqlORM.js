51133b94c2cedfb9a77a50d05b0dc807950e46b8
jamesxu <xxj8711@gmail.com>
2021-07-04 15:12:09 +0800

    login user: create userinfo in database and redis

---
+++ api/lib/MysqlORM.js
@@ -0,0 +1,131 @@
+const {Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(
+    process.env.MYSQL_DB, 
+    process.env.MYSQL_USER, 
+    process.env.MYSQL_PASS, {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    dialect: 'mysql'
+});
+
+sequelize.define('Push', {
+    id:{
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    openid: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: ""
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "",
+    },
+    desp: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        defaultValue: ""
+    },
+    readkey: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: ""
+    },
+    wxstatus:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: ""
+    }
+},
+{
+  tableName: 'push',
+  indexes:[
+    {
+        unique: false,
+        fields:['readkey']
+    },
+    {
+        unique: false,
+        fields:['openid']
+    }
+   ]
+});
+
+sequelize.define('User', {
+    id:{
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    nickname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: ""
+    },
+    openid: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "",
+        unique: true
+    },
+    session_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: ""
+    },
+    headimgurl: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: ""
+    },
+    level: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        defaultValue: 1
+    },
+    sendkey: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true
+    },
+    send_to_wechat: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        defaultValue: 1
+    },
+    send_to_ios: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        defaultValue: 1
+    },
+    send_to_android: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        defaultValue: 1
+    },
+    css_code: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        defaultValue: ""
+    },
+    js_code: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        defaultValue: ""
+    }
+  },
+  {
+    tableName: 'user'
+});
+
+(async ()=>
+{
+    await sequelize.models.User.sync();
+    await sequelize.models.Push.sync();
+})();
+
+
+module.exports =  sequelize;
--- api/routes/system.js
+++ api/routes/system.js
@@ -14,36 +14,58 @@ const wechat_config = {
   checkSignature: false
 }
 
-/* 获取微信登入的二维码. */
-
+/* 监听前端页面请求：获取微信登入的二维码. */
 router.get('/qrcode', async function(req, res, next) {
-  // res.send('respond with a resource');
-
-  // 生成一个唯一的 session_id
   // 首先通过 uuid 生成一个唯一的 session id 
   const session_id = 'sc2-'+uuid.v1();
-  
+
   // 然后将用户的 level 设置为 0
   const user_info = {"token":session_id,"level":0};
   // 并保存到 redis 中
   await req.redis.set("session/"+session_id , JSON.stringify(user_info));
-
   // 然后调用微信 api 生成二维码
   const qrdata = await api.createTmpQRCode(session_id,1800);
   qrdata.token = session_id;
   res.json( qrdata );
 });
 
-// 接口配置信息 正确响应微信发送的Token验证
-/*
-router.all('/wechat/callback', async (req, res, next) => {
-  
-  console.log(req.query);
-  res.send(req.query.echostr);
-})
-*/
-router.all('/wechat/callback', Wechat( wechat_config, async (req, res, next) => {
+//1> 对微信测试帐号的 URL 配置，校验前面之后，返回 echostr
+router.get('/wechat/callback', Wechat( wechat_config, async (req, res, next) => {
 
-  console.log(req.weixin);
+  // console.log(req.weixin);
+  res.send( req.query.echostr );
+}));
+
+// 2> 对微信推送的处理路由
+
+router.post( '/wechat/callback' , Wechat( wechat_config , async (req, res, next )=>
+{
+  // 响应事件
+  const message = req.weixin;
+  
+  if( message.MsgType == 'event' && (message.Event.toLowerCase() == 'scan' || message.Event.toLowerCase() == 'subscribe'  ) )
+  {
+    const session_id = message.Event.toLowerCase() == 'subscribe' ? message.EventKey.substr(8):message.EventKey;
+
+    const openid = message.FromUserName;
+    const user = await api.getUser(openid);
+    if( user )
+    {
+      const orm = require("../lib/MysqlORM");
+      const user_info = {"openid":openid,"session_id":session_id,"nickname":user.nickname,"headimgurl":user.headimgurl};
+      console.log(user_info);
+      const user_line = await orm.models.User.upsert( user_info);
+      
+      // 然后将 user信息放入 redis
+      req.redis.set('session/'+session_id , JSON.stringify(await orm.models.User.findOne({"where":{"id":user_line[0].dataValues.id}})));
+
+      // 发送扫码成功提示
+      res.reply("正在登入【思考空气网站】，请回到浏览器点击按钮继续");
+    }
+    else
+    res.reply("出现错误，请稍后再试");
+  }
+  
+
 }));
 module.exports = router;
